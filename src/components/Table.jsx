3cc58103d8d81390d11ad49e617efa4bd6240da7
masudur0823 <masudur0823@gmail.com>
2023-01-20 11:35:55 +0600

    edited

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function Table() {
   return (
-    <div>Table</div>
-  )
+   <div className="mt-4">
+     <table>
+      <tr>
+        <th>Alert Type</th>
+        <th>Cam ID</th>
+        <th>Location</th>
+        <th>Date</th>
+        <th>Image</th>
+        <th>Action</th>
+      </tr>
+      <tr>
+        <td>xyz</td>
+        <td>D1</td>
+        <td>Pakistan</td>
+        <td>2020-05-02 07:10:15</td>
+        <td>img</td>
+        <td><span>delete</span></td>
+        <td></td>
+      </tr>
+      
+    </table>
+   </div>
+  );
 }
 
-export default Table
+export default Table;
