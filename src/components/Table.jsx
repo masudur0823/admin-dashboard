e8e38a4531236186af03870be465b8d3853a1586
masudur0823 <masudur0823@gmail.com>
2023-01-20 14:34:18 +0600

    edited

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,28 +2,41 @@ import React from "react";
 
 function Table() {
   return (
-   <div className="mt-4">
-     <table>
-      <tr>
-        <th>Alert Type</th>
-        <th>Cam ID</th>
-        <th>Location</th>
-        <th>Date</th>
-        <th>Image</th>
-        <th>Action</th>
-      </tr>
-      <tr>
-        <td>xyz</td>
-        <td>D1</td>
-        <td>Pakistan</td>
-        <td>2020-05-02 07:10:15</td>
-        <td>img</td>
-        <td><span>delete</span></td>
-        <td></td>
-      </tr>
-      
-    </table>
-   </div>
+    <div className="mt-4">
+      <table>
+        <tr>
+          <th>
+            <label class="check_container">
+              <input type="checkbox" />
+              <span class="checkmark"></span>
+            </label>
+          </th>
+          <th>Alert Type</th>
+          <th>Cam ID</th>
+          <th>Location</th>
+          <th>Date</th>
+          <th>Image</th>
+          <th>Action</th>
+        </tr>
+        <tr>
+          <td>
+          <label class="check_container">
+              <input type="checkbox" />
+              <span class="checkmark"></span>
+            </label>
+          </td>
+          <td>xyz</td>
+          <td>D1</td>
+          <td>Pakistan</td>
+          <td>2020-05-02 07:10:15</td>
+          <td>img</td>
+          <td>
+            <span>delete</span>
+          </td>
+          <td></td>
+        </tr>
+      </table>
+    </div>
   );
 }
 
